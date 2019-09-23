5f8f693130862301902c03f9441bfdad065dbde7
Bryan Hunter <bryan.hunter@hcahealthcare.com>
2019-09-23 14:35:21 -0500

    Version bumps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MLLP.MixProject do
     [
       app: :elixir_mllp,
       version: String.trim(File.read!("./VERSION")),
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,7 +22,7 @@ defmodule MLLP.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ranch, "~> 1.6"},
+      {:ranch, "~> 1.7"},
       {:elixir_hl7, "~> 0.3.6"},
       {:dialyxir, "~> 0.5.1", only: [:dev], runtime: false},
       {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false},
