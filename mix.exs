bb4322ed8cc72d8bd147129916fd8098bb4820b6
Bryan Paxton <bryan@starbelly.io>
2023-10-13 13:08:35 -0500

    Bump HL7 to 0.7
    - Updated MLLP.Ack to import new sigil for use with get_part/2
    - Updated tests to reflect updates between 0.6.2 and 0.7.0

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule MLLP.MixProject do
     [
       {:telemetry, "~> 1.0"},
       {:ranch, "~> 1.8.0"},
-      {:elixir_hl7, "~> 0.6.0"},
+      {:elixir_hl7, "~> 0.7.0"},
       {:backoff, "~> 1.1.6"},
       {:ex_doc, "~> 0.24.2", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false},
