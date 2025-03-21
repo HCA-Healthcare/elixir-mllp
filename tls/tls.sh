6a4f5e0b3b315adbb883300d4d6cb1761bee4010
Vikas Bhardwaj <vikas.bhardwaj@hcahealthcare.com>
2025-03-21 12:18:08 -0500

    Ignorning case when comparing peer cert common name with allowed clients (#87)
    * Ignorning case when comparing peer cert common name with allowed clients. In some cases, we found certs are issued with mixed case CN and doing a case sensitive match caused issues. hostname verification worked fine but since :public_key.pkix_subject_id normalizes subject before returning it caused issue down the line.
    
    * updated cache action version
    
    ---------
    
    Co-authored-by: IUG8399 <iug8399@IUG8399s-MacBook-Pro.local>

--- tls/tls.sh
+++ tls/tls.sh
@@ -1,6 +1,6 @@
 cd tls
 
-mkdir root-ca client server expired_client
+mkdir root-ca client client_mixed_case_cn server expired_client
 cd root-ca
 rm index.txt
 touch index.txt
@@ -35,6 +35,17 @@ openssl req -new -key private_key.pem -out req.pem -outform PEM \
 cd ../root-ca
 openssl ca -config ../openssl.cnf -in ../client/req.pem -out \
     ../client/client_certificate.pem -notext -batch -extensions client_ca_extensions
+
+#Client Cert with mixed case CN
+cd ../client_mixed_case_cn
+
+openssl genrsa -out private_key.pem 2048
+openssl req -new -key private_key.pem -out req.pem -outform PEM \
+    -subj /CN=MIXED-CASE-client-cert/O=client/ -nodes
+
+cd ../root-ca
+openssl ca -config ../openssl.cnf -in ../client_mixed_case_cn/req.pem -out \
+    ../client_mixed_case_cn/client_certificate.pem -notext -batch -extensions client_ca_extensions
 
 #Client Expired Cert
 cd ../expired_client
